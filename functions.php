c4dff38a44755c18113333378ccfedc4d6f442f3
exactprecisions <exactprecisions@gmail.com>
2017-04-15 21:29:07 -0500

    remove custom-header

--- functions.php
+++ functions.php
@@ -19,10 +19,7 @@ require get_template_directory() . '/inc/widgets.php';
  * Enqueue scripts & styles
  */
 require get_template_directory() . '/inc/enqueue.php';
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
+
 /**
  * Custom template tags for this theme.
  */
