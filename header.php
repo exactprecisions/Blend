497b9354ac721efd2a48b59da541ffe8db802bf2
exactprecisions <exactprecisions@gmail.com>
2017-04-15 21:18:01 -0500

    add motion ui + base custom-header + font awesome fonts

--- header.php
+++ header.php
@@ -22,14 +22,37 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_s' ); ?></a>
+
+    <header id="masthead" class="site-header" role="banner">
+        <div class="site-branding">
+            <?php
+            if ( is_front_page() && is_home() ) : ?>
+                <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            <?php else : ?>
+                <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                <?php
+            endif;
+            $description = get_bloginfo( 'description', 'display' );
+            if ( $description || is_customize_preview() ) : ?>
+                <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+                <?php
+            endif; ?>
+        </div><!-- .site-branding -->
+
+    </header><!-- #masthead -->
+
     <div class="title-bar" data-responsive-toggle="navigation" data-hide-for="medium">
         <button class="menu-icon" type="button" data-toggle="navigation"></button>
         <div class="title-bar-title">Menu</div>
     </div>
-    <div id="navigation" class="top-bar site-header">
+    <div id="navigation" class="top-bar">
         <nav id="site-navigation" class="top-bar-left main-navigation" role="navigation">
             <?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu', 'menu_class' => 'menu' ) ); ?>
         </nav><!-- #site-navigation -->
     </div><!-- #masthead -->
+    <?php if(get_header_image()) : ?>
+        <img class="custom-header" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+    <?php endif; ?>
 
     <div id="content" class="row site-content">
