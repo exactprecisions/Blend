cd4673b003a23a17e763d66f23851aa7e952916b
exactprecisions <exactprecisions@gmail.com>
2017-07-01 07:13:43 -0500

    update Foundation 6.4.1

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -11,7 +11,7 @@
 function _blend_scripts() {
     wp_enqueue_style( '_blend-blend-style', get_template_directory_uri() . '/css/blend.min.css', '', '0.0.1' );
 	wp_enqueue_style( '_blend-custom-style', get_template_directory_uri() . '/css/custom.css', '', '0.0.1' );
-    wp_enqueue_script( '_blend-foundation-script', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '6.3.1', true );
+    wp_enqueue_script( '_blend-foundation-script', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '6.4.1', true );
     wp_enqueue_script( '_blend-theme-script', get_template_directory_uri() . '/js/theme.js', array('jquery'), '0.0.1', true );
 
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
